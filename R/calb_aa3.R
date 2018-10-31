b3fa99b5dd05e312328a1a84361e9cb04b631857
RConotte <raphael.conotte@outlook.com>
2018-10-31 13:33:26 +0100

    modification calb_aa3 function

--- R/calb_aa3.R
+++ R/calb_aa3.R
@@ -19,7 +19,7 @@
 #' @importFrom chart chart
 #'
 #' @examples
-#' # aa3_combine <- convert_aa3("inst/extra_data/181018E.TXT", "inst/extra_data/181018E.xlsx")
+#' # aa3_combine <- convert_aa3("inst/extra_data/181018E.TXT", "inst/extra_data/181018E.xlsx", project = "test")
 #' # filter_list <- list(Ptot =  c(50, 25), Ntot = c(0.1, 0.5, 1, 2))
 #' # plot_aa3(aa3_combine)
 #' # calb_aa3(aa3_combine, filter_list)
@@ -32,10 +32,9 @@ calb_aa3 <- function(aa3_combine, filter_list) {
   }
 
   # Check_2 : filter_list
-  nutrient_ctrl <- c("Ptot", "NO2", "NOx", "Ntot", "NH4", "PO4")
-
   if ( !is.null(filter_list) ) {
-    if ( sum(!(names(filter_list) %in% nutrient_ctrl)) != 0 ) {
+    if ( sum(!(names(filter_list) %in% attr(aa3_combine,
+                                            which = "method")$method)) != 0 ) {
       stop("Attention : pas de noms pour les elements de la liste ou pas de
            correspondance, utiliser un ou plusieurs des noms suivants :
            'Ptot', 'NO2', 'NOx', 'Ntot', 'NH4', 'PO4'")
@@ -55,13 +54,6 @@ calb_aa3 <- function(aa3_combine, filter_list) {
 
   # attribute_list
   attribute_list <- attributes(aa3_combine)
-
-  # unit vect
-  unit_v <- stringr::str_detect(attr(aa3_combine, which = "method")$unit,
-                                   "\u03BCmol/L")
-  unit_vect <- c()
-  unit_vect[unit_v] <- "\u03BCmol/L"
-  unit_vect[!unit_v] <- attr(aa3_combine, which = "method")$unit[!unit_v]
 
   for (i in 1:length(values)) {
 
@@ -104,30 +96,6 @@ calb_aa3 <- function(aa3_combine, filter_list) {
 
       names(lm_list)[i] <- paste(nutri_name)
 
-      # Equation
-      eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
-                       list(a = format(lm_mod$coefficients[[1]], digits = 2),
-                            b = format(lm_mod$coefficients[[2]], digits = 2),
-                            r2 = format(summary(lm_mod)$r.squared, digits = 3)))
-      eq <- as.character(as.expression(eq))
-
-      # lm graph
-      chart::chart(calb,
-                   formula = stats::as.formula(paste(values[i] ,"~", std[i])),
-                   na.rm = TRUE) +
-        geom_point(na.rm = TRUE) +
-        geom_smooth(method = "lm", na.rm = TRUE) +
-        ggtitle(stringr::str_split(values[i], pattern = "_")[[1]][1]) +
-        geom_text(x = 2*(diff(range(calb[[std[i]]], na.rm = TRUE)))/5 ,
-                  y = max( calb[[values[i]]] ),
-                  label = eq, parse = TRUE) +
-        xlab(paste0(nutri_name," [",unit_vect[i], "]")) +
-        ggrepel::geom_text_repel(aes(label = calb[[std[i]]]), na.rm = TRUE,
-                                 nudge_y = 1.5, nudge_x = 1.5,
-                                 direction = "both",
-                                 segment.size = 0.2) -> graphe
-      print(graphe)
-
       # add new nutrient values
       cnum <- which(names(aa3_combine) == conc[i])
       names(aa3_combine)[cnum] <- paste(conc[i], "old", sep = "_")
@@ -141,24 +109,7 @@ calb_aa3 <- function(aa3_combine, filter_list) {
 
     } else {
 
-      # lm ... IF no filter
-      # Check n(std)
-      if (sum( !is.na(aa3_combine[aa3_combine$sample_type == "CALB",
-                                  std[i]]) ) <= 3) {
-        stop("Attention seulement 3 points pour le calcul de la regression")
-      }
-
-      lm_mod <- stats::lm(aa3_combine,
-                          formula = stats::as.formula(paste(values[i],"~",std[i])))
-
-      data.frame(std_name = paste(nutri_name),
-                 intercept = lm_mod$coefficients[[1]],
-                 values = lm_mod$coefficients[[2]],
-                 r_squared = round(summary(lm_mod)$r.squared,digits = 3),
-                 n = dplyr::filter(aa3_combine, sample_type == "CALB") %>.%
-                   dplyr::select(., std[i]) %>.%
-                   sum(!is.na(.))
-                 ) ->  lm_list[[i]]
+      lm_list[[i]] <- attribute_list$calb_lm[i,]
 
       names(lm_list)[i] <- paste(nutri_name)
 
@@ -172,8 +123,9 @@ calb_aa3 <- function(aa3_combine, filter_list) {
 
   attr(aa3_combine, "class") <- c("aa3", "data.frame")
   attr(aa3_combine, "method") <- attribute_list$method
+  attr(aa3_combine, "calb_lm") <- lm_df
+  attr(aa3_combine, "calb_lm_old") <- attribute_list$calb_lm
   attr(aa3_combine, "metadata") <- attribute_list$metadata
-  attr(aa3_combine, "lm_df") <- lm_df
 
   return(aa3_combine)
 }
